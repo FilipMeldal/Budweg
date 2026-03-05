c40c62e7f7ce771d3bfbe91ffcff74e10882789b
spykiller103 <nbje74801@edu.ucl.dk>
2026-03-05 13:24:11 +0100

    Opdateret server configuration navnet til budweg

--- Budweg/Code/Budweg/Dashboard/Models/InspectionRepo.cs
+++ Budweg/Code/Budweg/Dashboard/Models/InspectionRepo.cs
@@ -1,17 +1,21 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Dashboard.Models
 {
-    public class InspectionRepo
+    public class InspectionRepo : DatabaseConnector
     {
         private Inspection inspection;
 
 
         public void InspecAdd(Inspection inspec)
         {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
 
+            }
         }
 
         public List<Inspection> InspecGetAll()
