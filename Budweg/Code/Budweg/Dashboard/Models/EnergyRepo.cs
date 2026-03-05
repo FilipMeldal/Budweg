f4d9092081b66ee7133a79572009708c6019cd5f
Rexiedexy <jlundgren07@gmail.com>
2026-03-05 13:14:41 +0100

    EnergyGetAll method skabt

--- Budweg/Code/Budweg/Dashboard/Models/EnergyRepo.cs
+++ Budweg/Code/Budweg/Dashboard/Models/EnergyRepo.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data.SqlClient;
+
+
 
 namespace Dashboard.Models
 {
     public class EnergyRepo
     {
         private EnergyUse energyuse;
+        private string connectionString = config.GetConnectionString("Navn af json db fil");
+
 
         public void EnergyAdd(EnergyUse energy)
         {
@@ -16,7 +21,35 @@ namespace Dashboard.Models
 
         public List<EnergyUse> EnergyGetAll()
         {
+            List<EnergyUse> energyList = new List<EnergyUse>();
 
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT Watt FROM EnergyUse";
+                SqlCommand command = new SqlCommand(query, connection);
+                try
+                {
+                    SqlDataReader reader = command.ExectuteReader();
+                    while (reader.Read())
+                    {
+                        EnergyUse energy = new EnergyUse
+                        {
+                            Watt = reader.GetDouble(0)
+                        };
+                        energyList.Add(energy);
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Der var fejl ved hentelse af energidata{ex.Message}", ex);
+
+
+
+                }
+                return energyList;
+            }
         }
 
         public EnergyRemove(EnergyUse energy)
@@ -31,3 +64,5 @@ namespace Dashboard.Models
 
     }
 }
+
+
