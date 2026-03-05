87e87ce27592f3481cf97b82e9c77c754015cbbd
Rexiedexy <jlundgren07@gmail.com>
2026-03-05 14:45:19 +0100

    WasteGetById is now made i think

--- Budweg/Code/Budweg/Dashboard/Models/WasteRepo.cs
+++ Budweg/Code/Budweg/Dashboard/Models/WasteRepo.cs
@@ -31,8 +31,8 @@ namespace Dashboard.Models
                     {
                         Waste waste = new Waste
                         {
-                            WasteAmount = reader.GetDouble(1),
-                            Material = reader.GetString(2),
+                            WasteAmount = reader.GetDouble(0),
+                            Material = reader.GetString(1),
                         };
                         wasteList.Add(waste);
                     }
@@ -48,6 +48,37 @@ namespace Dashboard.Models
 
         public Waste WasteGetById(int id)
         {
+            Waste waste = null;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = @"SELECT w.WasteID, w.WasteAmount, w.Material, w.CaliperID 
+                                 FROM Waste w 
+                                 INNER JOIN BrakeCaliper b ON w.CaliperID = b.CaliperID 
+                                 WHERE w.WasteID = @WasteID";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@WasteID", id);
+                try
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        waste = new Waste
+                        {
+                            WasteID = reader.GetInt32(0),
+                            WasteAmount = reader.GetDouble(1),
+                            Material = reader.GetString(2),
+                            CaliperID = reader.IsDBNull(3) ? null : reader.GetInt32(3)
+                        };
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred while retrieving waste data: " + ex.Message);
+                }
+            }
+            return waste;
 
 
         }
