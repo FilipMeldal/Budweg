31736ef0af7cd88b8f011510ecf61bfed9294ae5
spykiller103 <nbje74801@edu.ucl.dk>
2026-03-11 15:39:41 +0100

    Lavet enums og fået dem ind i en combobox

---
+++ Budweg/Code/Budweg/Dashboard/Models/TimeInterval.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Dashboard.Models
+{
+    public enum TimeInterval
+    {
+        [Display(Name = "It's Now")]
+        Now,
+        OneDay,
+        ThreeDays,
+        OneMonth,
+        ThreeMonths,
+        SixMonths,
+        LastYear,
+        TwoYears,
+    }
+}
--- Budweg/Code/Budweg/Dashboard/ViewModels/MainViewModel.cs
+++ Budweg/Code/Budweg/Dashboard/ViewModels/MainViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using Dashboard.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Dashboard.ViewModels
 {
     public class MainViewModel
     {
-            
     }
 }
