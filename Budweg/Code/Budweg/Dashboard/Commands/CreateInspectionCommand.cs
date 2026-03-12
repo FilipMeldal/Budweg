4a774e133a5b096e7b5a762e68a38154b2159fd4
Jeppe Helsted <jeppe@helsted.com>
2026-03-12 13:37:55 +0100

    Oprettet commandbinding branch og startet command binding

---
+++ Budweg/Code/Budweg/Dashboard/Commands/CreateInspectionCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Dashboard.Commands
+{
+    public class CreateInspectionCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            
+        }
+    }
+}
---
+++ Budweg/Code/Budweg/Dashboard/Commands/SaveInspectionCommand.cs
@@ -0,0 +1,27 @@
+﻿using Dashboard.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Dashboard.Commands
+{
+    public class SaveInspectionCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (parameter is MainViewModel mvm)
+            {
+                mvm.SaveInspection();
+            }
+
+        }
+    }
+}
