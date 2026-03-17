0e750bc9004ca9d13f9ecf37cceebd5f1adacc9a
Jeppe Helsted <jeppe@helsted.com>
2026-03-17 12:54:17 +0100

    fixer command binding

--- Budweg/Code/Budweg/Dashboard/Views/DashBoardView.xaml.cs
+++ Budweg/Code/Budweg/Dashboard/Views/DashBoardView.xaml.cs
@@ -22,5 +22,10 @@ namespace Dashboard.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
